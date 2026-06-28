9982ebaa7d92c249d5a6e19e3f5a2e1437e82bd4
Tri250 <Tri250@users.noreply.github.com>
2026-06-28 08:53:17 +0000

    chore: bump version to 1.0.5 (5)
    - versionCode: 1 -> 5
    - versionName: 1.0.0 -> 1.0.5

--- RapidRAW-Android/app/build.gradle.kts
+++ RapidRAW-Android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.rapidraw"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
